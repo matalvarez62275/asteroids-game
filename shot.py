eccbe56f61e7f9c2f4679e30ceb9438823123ec2
matalvarez62275 <matalvarez@itba.edu.ar>
2025-08-30 15:38:28 +0000

    solid (filled) shots)

--- shot.py
+++ shot.py
@@ -8,7 +8,7 @@ class Shot(CircleShape):
         super().__init__(x, y, SHOT_RADIUS)
         
     def draw(self, screen: pygame.Surface):
-        pygame.draw.circle(screen, "red", self.position, self.radius, 2)
+        pygame.draw.circle(screen, "red", self.position, self.radius)
         
     def update(self, dt: float):
         self.position +=  self.velocity * dt
